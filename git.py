fc49377ae45b72be51d2c06c02300b4079788f58
katephc <beast-1986@mail.ru>
2022-06-04 14:22:35 +0300

    df

---
+++ git.py
@@ -0,0 +1,11 @@
+num = int(input())
+sum_nums = 0
+while num != 0:
+    last = num % 10
+    sum_nums += last
+    num = num // 10
+print(sum_nums)
+
+ls = [int(i) for i in input().split()]
+a = sum(ls)
+print(a)
